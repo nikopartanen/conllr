6033344baaed1cbddafbbf3b664b495ed1fefbeb
Niko Partanen <nikotapiopartanen@gmail.com>
2017-04-10 16:24:55 +0200

    this works now nicely

--- R/read_conll.R
+++ R/read_conll.R
@@ -8,22 +8,25 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-read_conll <- function(conll_file = 'https://raw.githubusercontent.com/UniversalDependencies/UD_Russian/master/ru-ud-dev.conllu'){
+read_conll <- function(conll_file = '~/langdoc/universal_dependencies/ud-treebanks-conll2017/UD_Russian-SynTagRus/ru_syntagrus-ud-train.conllu'){
 
   `%>%` <- dplyr::`%>%`
 
   conll_lines <- readr::read_lines(conll_file)
 
   dplyr::data_frame(string = conll_lines) %>%
+    dplyr::mutate(sentence = cumsum(stringr::str_detect(string, '^$'))) %>%
     dplyr::filter(! string == '') %>%
-    dplyr::mutate(sentence = cumsum(stringr::str_detect(string, '^# sent_id'))) %>%
     dplyr::group_by(sentence) %>%
-    dplyr::mutate(id = dplyr::first(string)) %>%
-    dplyr::mutate(id = stringr::str_extract(id, '(?<=# sent_id = )(.+)')) %>%
-    dplyr::slice(-1) %>%
-    dplyr::mutate(sentence_text = dplyr::first(string)) %>%
-    dplyr::mutate(sentence_text = stringr::str_extract(sentence_text, '(?<=# text = )(.+)')) %>%
-    dplyr::slice(-1) %>%
+#    dplyr::mutate(id = dplyr::first(string)) %>%
+    dplyr::mutate(id = stringr::str_extract(string, '(?<=# sent_id = )(.+)')) %>%
+#    dplyr::slice(-1) %>%
+#    dplyr::mutate(sentence_text = dplyr::first(string)) %>%
+    dplyr::mutate(sentence_text = stringr::str_extract(string, '(?<=# text = )(.+)')) %>%
+#    dplyr::slice(-1) %>%
+    tidyr::fill(id) %>%
+    tidyr::fill(sentence_text) %>%
+    dplyr::filter(! stringr::str_detect(string, '^#')) %>% # if there are more annotations above the sentence, those are now disgarded
     dplyr::ungroup() %>%
     tidyr::separate(col = string,
                     into = c('id', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc'),
